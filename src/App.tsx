4b322e603b3e325ed107310700ebb15f2ba73aa9
yettabaa <yassin.ettabaa1998@gmail.com>
2024-12-17 18:07:20 +0100

    first push

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div className='bg-teal-400'>
+        <p>hello</p>
+    </div>
+   
+  )
+}
+
+export default App
