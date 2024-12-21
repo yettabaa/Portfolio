cf14af15d3589ec330ff7d0e55f811b8885adc6d
yettabaa <yassin.ettabaa1998@gmail.com>
2024-12-21 01:41:29 +0100

    footer gif to project ans show the important projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import FdF from '../assets/FdF-Scale-Z.gif';
+import cub3D from '../assets/cub3D_1.gif';
+import inception from '../assets/schema.png';
+import bacNote from '../assets/Bac-Note-Calculator.gif';
 
 interface Repo {
     id: number; // Assuming 'id' is a unique identifier
@@ -9,6 +13,13 @@ interface Repo {
 
 const Projects : React.FC = () => {
     const [repos, setRepos] = useState<Repo[]>([]);
+    const [showMore, setShowMore] = useState(false);
+    const gifs : any = {
+        'FdF': FdF,
+        'cub3D': cub3D,
+        'Inception': inception,
+        'Bac-Note-Calculator': bacNote
+    }
     useEffect(() => {
         fetch('https://api.github.com/users/yettabaa/repos')
             .then(response => response.json())
@@ -19,28 +30,78 @@ const Projects : React.FC = () => {
     }
     , []);
 
+    let withGif:Repo[] = [];
+    let withoutGif:Repo[] = [];
+    repos.forEach((repo: Repo) => {
+        if (repo.name === 'Bac-Note-Calculator' || repo.name === 'FdF'
+            || repo.name === 'cub3D' || repo.name === 'Inception') {
+            withGif.push(repo);
+        } else if (repo.name !== 'yettabaa') {
+            withoutGif.push(repo);
+        }
+    });
+
     return (
         <div id='projects' className='flex flex-col gap-4 p-8'>
-            <span className='font-merriweather font-bold text-2xl text-_green'>
+            <span className='font-merriweather font-bold text-2xl text-_green pb-4'>
                 Projects
             </span>
+            <div className='flex flex-col gap-10'>
+                {
+                    withGif.map((repo : Repo) => {
+                        return (
+                            <div key={repo.id} className='flex flex-col gap-4 rounded-md overflow-hidden border-2 border-gray-500'>
+                                <img src={gifs[repo.name]} alt="instagram" className=" h-60 w-auto" />
+                                {/* <span className='font-merriweather font-medium text-xl text-_green'>
+                                    {repo.name}
+                                    </span>
+                                    <span className='font-merriweather font-normal text-white text-lg'>
+                                    {repo.description}
+                                    </span>
+                                    <a href={repo.html_url} className='font-merriweather font-normal text-white text-lg'>
+                                    {repo.html_url}
+                                    </a> */}
+                            </div>
+                        );
+                    })
+                }
+            </div>
             {
-                repos.map((repo : Repo) => {
-                    return (
-                        <div key={repo.id} className='flex flex-col gap-4 '>
-                            <span className='font-merriweather font-medium text-xl text-_green'>
-                                {repo.name}
-                            </span>
-                            <span className='font-merriweather font-normal text-white text-lg'>
-                                {repo.description}
-                            </span>
-                            <a href={repo.html_url} className='font-merriweather font-normal text-white text-lg'>
-                                {repo.html_url}
-                            </a>
-                        </div>
-                    );
-                })
-            }      
+
+                !showMore && <span className='font-merriweather font-bold '
+                onClick={() => setShowMore(!showMore)}
+                >
+                Show more ...
+                </span>
+            }
+            {
+
+                showMore && <span className='font-merriweather font-bold '
+                onClick={() => setShowMore(!showMore)}
+                >
+                Show less
+                </span>
+            }
+            <div className='flex flex-col gap-10'>
+                {
+
+                    showMore && withoutGif.map((repo: Repo) => {
+                        return (
+                            <div key={repo.id} className='flex flex-col gap-4 rounded-md overflow-hidden border-2 border-gray-500'>
+                                <span className='font-merriweather font-medium text-xl text-_green'>
+                                    {repo.name}
+                                </span>
+                                <span className='font-merriweather font-normal text-white text-lg'>
+                                    {repo.description}
+                                </span>
+                                <a href={repo.html_url} className='font-merriweather font-normal text-white text-lg'>
+                                    {repo.html_url}
+                                </a>
+                            </div>
+                        );
+                    })
+                }
+            </div>
         </div>
     );
 }
